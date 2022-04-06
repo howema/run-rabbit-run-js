fcf52dfa3b609531eea038b94b801f5894ff2b63
Meg Howe <meg@heysparkbox.com>
2022-04-06 08:56:13 -0700

    chore: relocate joson to api directory

---
+++ api/routes.js
@@ -0,0 +1,30 @@
+
+// const Hapi = require('@hapi/hapi'); //go into library
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello you';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
