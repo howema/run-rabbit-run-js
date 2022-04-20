47e895fde23ffb5ec67813b85913c1f0c53879d5
Meg Howe <meg@heysparkbox.com>
2022-04-20 08:56:20 -0700

    feat: add render 1 note within grid functionality
    info:
    -created li element to insert within div element
    -now render note inside grid
    -only grabbing first note for now
    
    completes FSA22V1-149

--- src/js/notes.js
+++ src/js/notes.js
@@ -1,17 +1,44 @@
 
 import notesList from "../../api/notes.json"
 
+// export function notes() {
+//     let note = document.createElement("div");
+//     note.className = "quote";
+//     note.classList.add("quote"); //.place, .toggle
+//     let noteText = document.createTextNode("words");
+    
+//     note.appendChild(noteText)
+//     const notesContainer = document.getElementById("notes-container");
+//     notesContainer.appendChild(note);
+//     return notesList;
+// }
+
 export function notes() {
     let note = document.createElement("div");
-    note.className = "quote";
-    note.classList.add("quote"); //.place, .toggle
-    let noteText = document.createTextNode("words");
+    note.className = "quotes";
+    note.classList.add("quotes"); //.place, .toggle
+
+    let noteLi = document.createElement("li");
+
+    let noteText = document.createTextNode(notesList[0].note);
     
-    note.appendChild(noteText)
+    note.appendChild(noteLi);
+    noteLi.appendChild(noteText)
+
     const notesContainer = document.getElementById("notes-container");
     notesContainer.appendChild(note);
+    // let quotesList = document.querySelectorAll(".quotes");
+    // quotesList.appendChild(note);
+    // console.log(quotesList);
+    // return quotesList;
     return notesList;
 }
+
+
+// function noteLooper(notesList) {
+
+
+// }
 
         
         // export function notes() {
